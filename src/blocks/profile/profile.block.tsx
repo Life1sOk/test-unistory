016d9b53bee0760276f6defed3e62541e58a14cd
Life1sOk <mr.lifeisok@gmail.com>
2023-03-10 00:17:08 +0600

    change spinner

--- src/blocks/profile/profile.block.tsx
+++ src/blocks/profile/profile.block.tsx
@@ -3,21 +3,32 @@ import { useParams } from 'react-router-dom';
 import { useGetViewParticipantQuery } from '../../app-redux/api/participantsAPI';
 
 import UserField from '../../components/user-field/user-field.component';
+import LoadingSpinner from '../../components/loading-spinner/loading-spinner.component';
 
+import { SpinnerWrapper } from '../../pages/index.style';
 import { ProfileStyle, ProfileStyleTitle } from './profile.style';
 
 const Profile = () => {
     let { id } = useParams();
 
-    const { data } = useGetViewParticipantQuery(Number(id), { skip: id == null });
+    const { data, isLoading } = useGetViewParticipantQuery(Number(id), { skip: id == null });
 
     return (
-        <ProfileStyle>
-            <ProfileStyleTitle>PERSONAL DATA</ProfileStyleTitle>
-            <UserField label='NAME' text={data?.username} />
-            <UserField label='EMAIL' text={data?.email} />
-            <UserField label='WALLET' text={data?.address} />
-        </ProfileStyle>
+        <>
+            {
+                isLoading ?
+                    <SpinnerWrapper>
+                        <LoadingSpinner />
+                    </SpinnerWrapper>
+                    :
+                    <ProfileStyle>
+                        <ProfileStyleTitle>PERSONAL DATA</ProfileStyleTitle>
+                        <UserField label='NAME' text={data?.username} />
+                        <UserField label='EMAIL' text={data?.email} />
+                        <UserField label='WALLET' text={data?.address} />
+                    </ProfileStyle>
+            }
+        </>
     )
 };
 
--- src/pages/profile/profile.page.tsx
+++ src/pages/profile/profile.page.tsx
@@ -1,29 +1,15 @@
 import Profile from '../../blocks/profile/profile.block';
 import Planet from '../../blocks/planet/planet.block';
-import LoadingSpinner from '../../components/loading-spinner/loading-spinner.component';
-
-import { useGetViewParticipantQuery } from '../../app-redux/api/participantsAPI';
 
 import { ProfilePageStyle } from './profile.style';
-import { SpinnerWrapper } from '../index.style';
 
 const ProfilePage = () => {
-    const { isLoading } = useGetViewParticipantQuery();
 
     return (
-        <>
-            {
-                isLoading ?
-                    <SpinnerWrapper>
-                        <LoadingSpinner />
-                    </SpinnerWrapper>
-                    :
-                    <ProfilePageStyle>
-                        <Profile />
-                        <Planet pointBox={false} top={46} left={1113} ellipse={false} />
-                    </ProfilePageStyle>
-            }
-        </>
+        <ProfilePageStyle>
+            <Profile />
+            <Planet pointBox={false} top={46} left={1113} ellipse={false} />
+        </ProfilePageStyle>
     )
 };
 
