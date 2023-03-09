af60285f719b20140433572999f20dcba22a2c3d
Life1sOk <mr.lifeisok@gmail.com>
2023-03-09 16:03:49 +0600

    fix

--- src/components/text-box/text-box.component.tsx
+++ src/components/text-box/text-box.component.tsx
@@ -4,7 +4,7 @@ import { FC, memo } from "react";
 import { TextBoxStyle } from './text-box.style';
 
 const TextBox: FC<{ text: string }> = memo(({ text }) => {
-    console.log('rerun')
+
     return (
         <TextBoxStyle>{text}</TextBoxStyle>
     )
